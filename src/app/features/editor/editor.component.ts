a571378fae7412e7b077d8254e01ed4128d2bb52
NazarenoTognoli <nazarenotognoli@gmail.com>
2025-02-25 15:33:28 -0300

    added another input in the editor for changing the title

--- src/app/features/editor/editor.component.ts
+++ src/app/features/editor/editor.component.ts
@@ -1,4 +1,6 @@
 import { Component, AfterViewInit, ViewChild, ElementRef, effect } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
 import { ItemsStateService } from '@app/core/items-state.service';
 import { ItemsSyncService } from '@app/core/items-sync.service';
 import { generateId } from '@app/shared/models/item.model';
@@ -6,46 +8,70 @@ import { generateId } from '@app/shared/models/item.model';
 @Component({
   selector: 'app-editor',
   standalone: true,
-  imports: [],
+  imports: [CommonModule, FormsModule],
   templateUrl: './editor.component.html',
   styleUrl: './editor.component.scss'
 })
 export class EditorComponent implements AfterViewInit {
   @ViewChild('editor') editorElement!: ElementRef<HTMLTextAreaElement>;
   viewInit = false;
+  titleInputValue:string = "";
 
   constructor(public itemsState:ItemsStateService, private itemsSync:ItemsSyncService){
     effect(()=>{
-      if (this.viewInit && (this.itemsState.editor() || this.itemsState.creation())){ this.editorElement.nativeElement.value = this.previousData() }
+      if (this.viewInit && (this.itemsState.editor() || this.itemsState.creation())){ this.editorElement.nativeElement.value = this.previousData(); this.titleInputValue = this.previousData(true) }
     });
   }
+  onTitleInput(event:Event){
+    const inputValue = (event.target as HTMLInputElement).value;
+    this.itemsState.editor() && this.handleEdition(inputValue, true);
+    this.itemsState.creation() && this.handleCreation(inputValue, true);
+  }
 
-  previousData():string {
-    if(this.itemsState.editor()){
+  previousData(title:boolean = false):string {
+    if(this.itemsState.editor() && !title){
       return this.itemsState.editorData().content;
     }
-    else if (this.itemsState.creation() && this.itemsState.creationData()){
+    else if (this.itemsState.creation() && this.itemsState.creationData() && !title){
       return this.itemsState.creationData().content;
+    }
+    else if (this.itemsState.editor() && title){
+      return this.itemsState.editorData().title;
+    }
+    else if (this.itemsState.creation() && this.itemsState.creationData() && title){
+      return this.itemsState.creationData().title;
     }
     else {
       return "";
     }
   }
 
-  handleEdition(contentValue:string){
-    const data = () => ({...this.itemsState.editorData(), content:contentValue, modificationDate:new Date().toString()});
+  handleEdition(contentValue:string, title:boolean = false){
+    const data = () => ({
+      ...this.itemsState.editorData(), 
+      content:!title ? contentValue : this.itemsState.editorData().content,
+      title:!title ? this.itemsState.editorData().title : contentValue,
+      modificationDate:new Date().toString()
+    });
     this.itemsState.editorData.set(data());
   }
 
-  handleCreation(contentValue:string){
+  handleCreation(contentValue:string, title:boolean = false){
     const date = new Date().toString();
-    const data = () => ({id:generateId(), modificationDate: date, title: "title undefined", content:contentValue});
+    const creationData = this.itemsState.creationData() ?? {content:"", title:"title undefined"};
+    const data = () => ({
+      ...creationData,
+      id: creationData.id ?? generateId(),
+      modificationDate: date,
+      title: title ? contentValue : creationData.title,
+      content:!title ? contentValue : creationData.content});
     this.itemsState.creationData.set(data());
   }
 
   ngAfterViewInit() {
     this.viewInit = true;
     this.editorElement.nativeElement.value = this.previousData();
+    this.titleInputValue = this.previousData(true);
     this.editorElement.nativeElement.addEventListener('input', (event:Event)=>{
       const contentValue = (event.target as HTMLTextAreaElement).value;
       this.itemsState.editor() ? this.handleEdition(contentValue) : undefined;
